956c32933aaf2fc8248ee7b0647737d6c199d888
Marcos Alves <82622925+marcosouza97@users.noreply.github.com>
2021-07-02 02:07:17 +0000

    estrutura.2

--- main.cpp
+++ main.cpp
@@ -78,13 +78,33 @@ void incluirNoFim(tLista* pLista, int info){
 
 void imprimirLista(tLista* pLista){
   pLista->marcador = pLista->primeiro;
+  
 
   while(!finalLista(pLista)){
   int informacao = pLista->marcador->info;
   cout << "A informacao eh:" << informacao<<endl;
+
   
   pLista ->marcador = pLista->marcador->proximo;
   }
+}
+
+void varrerLista(tLista* pLista){
+  
+  pLista->marcador = pLista->primeiro;
+  
+  ofstream gravar;
+  gravar.open("valores.txt",ios::out);
+
+  while(!finalLista(pLista)){
+  int informacao = pLista->marcador->info;
+
+  gravar << informacao <<endl;
+
+  pLista ->marcador = pLista->marcador->proximo;
+
+  }
+  gravar.close();
 }
 
 
@@ -95,51 +115,51 @@ tTriangulo base, altura, hipotenusa;
 
 inicializaLista (lados);
 
-// entrada de dados
-
-cout << "Difite uma opção:" << endl;
+cout << "Digite uma opção:" << endl;
 cout << "1 - para ler" << endl;
 cout << "2 - para escrever" << endl;
 cin >> aux1;
 
-if(aux1 == 1){
-
-}
-else{
-  if (aux1 == 2){
-    cout << "Digite quantos triangulos deseja gravar" << endl;
-    cin >> aux2;
-
-
-    for(int i=0;i<aux2;i++){
-      
-      cout << "Digite o" << i+1 <<"° lado" << endl;
-      cin >> base.lado1;
-      incluirNoFim(lados, base.lado1);
-
-
-      cout << "Digite o " << i+1 <<"° lado" <<endl;
-      cin >> altura.lado2;
-      incluirNoFim(lados, altura.lado2);
-
-
-      cout << "Digite o " << i+2 << "° lado" << endl;
-      cin >> hipotenusa.lado3;
-      incluirNoFim(lados, hipotenusa.lado3);
-
-
-    }
-
-    imprimirLista(lados);
-
-
-
-
+// entrada de dados
+while(aux1!=){
+  
+  if(aux1 == 1){
 
   }
+  else{
+    if (aux1 == 2){
+      cout << "Digite quantos triangulos deseja gravar" << endl;
+      cin >> aux2;
+
+
+      for(int i=0;i<aux2;i++){
+        
+        cout << "Digite o" << i+1 <<"° lado" << endl;
+        cin >> base.lado1;
+        incluirNoFim(lados, base.lado1);
+
+
+        cout << "Digite o " << i+1 <<"° lado" <<endl;
+        cin >> altura.lado2;
+        incluirNoFim(lados, altura.lado2);
+
+
+        cout << "Digite o " << i+2 << "° lado" << endl;
+        cin >> hipotenusa.lado3;
+        incluirNoFim(lados, hipotenusa.lado3);
+
+
+      }
+
+      imprimirLista(lados);
+      varrerLista (lados);
+    }
+  }
+  cout << "O que deseja fazer" <<endl;
+  cout << "1 - para ler" << endl;
+  cout << "2 - para escrever" << endl;
+  cin >> aux1;
 }
-
-
 
 
 
